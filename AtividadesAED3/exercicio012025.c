a4dd40f8d499bc43eb8ab02dcc3538804dedf849
Nephelym <148366524+MatheusPardiAlmeida@users.noreply.github.com>
2025-06-19 18:11:11 -0400

    Add files via upload

--- AtividadesAED3/exercicio012025.c
+++ AtividadesAED3/exercicio012025.c
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
 typedef struct Node{
     int valor;
     struct Node *left;
     struct Node *right;
     struct Node *pai;
 } Node;
+
 
 Node* criarNode(int valor){
     Node *ponto = (Node*)malloc(sizeof(Node));
@@ -21,8 +21,8 @@ Node* criarNode(int valor){
     return ponto;
 }
 
-void inserirElemento(Node **raiz, Node **atual) {
-
+void inserirElemento(Node **raiz, Node **atual){
+ // Utilizamos ponteiro duplo caso a função precise alterar as variáveis.
     int escolha;
     int valor;
 
@@ -30,20 +30,20 @@ void inserirElemento(Node **raiz, Node **atual) {
     scanf("%d", &valor);
     getchar();
 
-    if (valor < 0 || isdigit(valor)) {
+    if (valor < 0 || isdigit(valor)) { //Verifica se o valor é negativo ou não é um número inteiro
         printf("\nValor invalido, digite novamente: ");
         scanf("%d", &valor);
         getchar();
     }
 
-    Node *ponto = criarNode(valor);
+    Node *ponto = criarNode(valor); //Cria o node
 
-    if (*raiz == NULL){
-        *raiz = ponto;
-        *atual = ponto;
+    if (*raiz == NULL){ //Caso o node seja o primeiro, será a raiz
+        *raiz = ponto; // Atribui o node criado como raiz
+        *atual = ponto; // Atribui o node criado como atual
     }
-    else{
-        
+    else{ //Caso contrário
+    
         int i = 0;
 
         while (i == 0)
@@ -59,7 +59,7 @@ void inserirElemento(Node **raiz, Node **atual) {
             {
                 case 1:
                 if ((*atual)->left == NULL){
-                    ponto->pai = *atual;
+                    ponto->pai = *atual; // Para facilitar na hora de subir de nível, todos os nós subsequentes terão um valor pai atribuído a elas
                     (*atual)->left = ponto;
                     i = 1;
                 }
@@ -75,7 +75,7 @@ void inserirElemento(Node **raiz, Node **atual) {
 
                 case 2:
                 if ((*atual)->right == NULL){
-                    ponto->pai = *atual;
+                    ponto->pai = *atual; // Para facilitar na hora de subir de nível, todos os nós subsequentes terão um valor pai atribuído a elas
                     (*atual)->right = ponto;
                     i = 1;
                 }
@@ -102,48 +102,25 @@ void inserirElemento(Node **raiz, Node **atual) {
     return;
 }
 
-void elementoAtual(Node **atual){
+void percorrerArvore(Node **atual, Node *raiz) { // Como a função só altera o endereço de memória de atual, raiz não precisa ser um ponteiro duplo, apenas teremos seu valor como referência.
 
-    printf("sduiafhihousaiohuasihuodfsahuidsa\n");
-
-    if (*atual == NULL){
-        printf("\nO elemento atual se encontra vazio.\n");
-        printf("Pressione qualquer tecla para continuar...");
-        getchar();
-    }
-    else{
-        printf("\nO elemento atual:\n");
-        printf("%d\n", (*atual)->valor);
-
-            if (!((*atual)->left))
-            {
-                printf("\tEsquerda: #\n");
-            }
-            else{
-                printf("\tEsquerda: %d\n", (*atual)->left->valor);
-            }
-            if (!((*atual)->right))
-            {
-                printf("\tDireita: #\n");
-            }
-            else{
-                printf("\tDireita: %d\n", (*atual)->right->valor);
-            }
-    }
-
-    system("cls");
-
-}
-
-void percorrerArvore(Node **atual, Node **raiz) {
-
-    if (*atual == NULL) 
+    if (*atual == NULL && raiz == NULL) // Caso tanto o valor atual quanto a raiz estejam vazias, a árvore está completamente vazia
     {
-        printf("\nA arvore esta vazia.\n");
-        printf("Pressione qualquer tecla para continuar...");
+        printf("\nA arvore esta vazia.");
+        printf("\nPressione qualquer tecla para continuar...");
         getchar();
+        system("cls");
         return;
     }
+    // else if (*atual == NULL && raiz != NULL) // Caso o ponteiro do valor atual esteja apontando para um endereço NULL mas a raiz não, então retornamos o atual para a raiz
+    //{
+        //*atual = raiz;
+        //printf("\nO elemento atual se encontra vazio, retornando a raiz.");
+        //printf("\nPressione qualquer tecla para continuar...");
+        //getchar();
+        //system("cls");
+        //return;
+    //}
 
     int opcao;
 
@@ -159,18 +136,45 @@ void percorrerArvore(Node **atual, Node **raiz) {
     switch (opcao)
     {
     case 1:
-        *atual = (*atual)->left;
+        if (((*atual)->left == NULL)){ // Impede o usuário de ir para um endereço de memória NULL
+            printf("\nO elemento a esquerda esta vazio. Pressione qualquer tecla para continuar.");
+            getchar();
+            system("cls");
+            return;
+        }
+        else{
+            *atual = (*atual)->left;
+        }
+        
         break;
     case 2:
-        *atual = (*atual)->right;
+        if (((*atual)->right == NULL)){ // Impede o usuário de ir para um endereço de memória NULL
+            printf("\nO elemento a direita esta vazio. Pressione qualquer tecla para continuar.");
+            getchar();
+            system("cls");
+            return;
+        }
+        else{
+            *atual = (*atual)->right;
+        }
         break;
     case 3:
-        *atual = *raiz;
+        if (*atual == raiz){
+            printf("\nA arvore ja esta na raiz, pressione qualquer tecla para continuar.");
+            getchar();
+            system("cls");
+            return;
+        }
+        else{
+            *atual = raiz;
+        }
+        
         break;
     case 4:
         if ((*atual)->pai == NULL){
             printf("\nA arvore ja esta no topo, pressione qualquer tecla para continuar.");
             getchar();
+            system("cls");
             return;
         }
         else{
@@ -190,22 +194,37 @@ void percorrerArvore(Node **atual, Node **raiz) {
     return;
 }
 
-void excluirNodo(Node ***p){
+void excluirNodo(Node **p){
     if(p != NULL){
-        if((**p)->left != NULL)
-            excluirNodo(&((**p)->left));
-        if((**p)->right != NULL)
-            excluirNodo(&((**p)->right));
-        free(*p);
+        if((*p)->left != NULL){
+            excluirNodo(&(*p)->left); // Chama a função recursiva e verifica todos os elementos a esquerda do valor que será excluido
+        }
+        else{
+            printf("\nElemento a esquerda vazio.\n"); // Para fazer verificação se esta funcionando corretamente
+        }
+        if((*p)->right != NULL){
+            excluirNodo(&(*p)->right); // Chama a função rescursiva e verifica todos os elementos a direita do valor que será excluido
+        }
+        else{
+            printf("\nElemento a direita vazio.\n"); // Para fazer verificação se esta funcionando corretamente
+        }
+        free(*p); // Libera o espaço de memória
     }
+    else{
+        printf("\nO elemento a ser removido nao existe");
+        printf("\nPressione qualquer tecla para continuar...");
+        getchar();
+    }
+    return;
 }
 
-void removerElemento(Node **atual, Node **raiz) {
+void removerElemento(Node **atual, Node **raiz) {  // Utilizamos ponteiro duplo caso a função precise alterar as variáveis.
 
-    if (*atual == NULL) {
+    if (*atual == NULL) { // Caso o valor atual seja nulo, então a árvore está vazia 
         printf("\nNao tem elementos presentes.");
         printf("\nPressione qualquer tecla para continuar...");
         getchar();
+        system("cls");
         return;
     }
 
@@ -215,19 +234,21 @@ void removerElemento(Node **atual, Node **raiz) {
         while (i == 0)
         {
             printf("\nInforme onde deseja remover o valor:");
-            printf("\n1 - Esquerda\n");
-            printf("2 - Direita\n");
-            printf("Digite sua escolha: ");
+            printf("\n1 - Esquerda");
+            printf("\n2 - Direita");
+            printf("\nDigite sua escolha: ");
             scanf("%d", &escolha);
             getchar();
 
-            if ((*atual == *raiz))
+            if (*atual == *raiz) // Caso o endereço de memória atual seja o mesmo da raiz, terá a opção de excluir a raiz
             {
+                system("cls");
+                printf("\nA arvore esta na raiz.");
                 printf("\nInforme onde deseja remover o valor:");
-                printf("\n1 - Esquerda\n");
-                printf("2 - Direita\n");
-                printf("3 - Raiz\n");
-                printf("Digite sua escolha: ");
+                printf("\n1 - Esquerda");
+                printf("\n2 - Direita");
+                printf("\n3 - Raiz");
+                printf("\nDigite sua escolha: ");
                 scanf("%d", &escolha);
                 getchar();
             }
@@ -236,22 +257,32 @@ void removerElemento(Node **atual, Node **raiz) {
             switch (escolha)
             {
                 case 1:
-                excluirNodo(&((*atual)->left));
-                (*atual)->left = NULL;
-
-                break;
+                excluirNodo(&(*atual)->left); // Chama a função recursiva
+                (*atual)->left = NULL; // Deixa o endereço de memória a esquerda do atual como NULL
+                printf("\nOs elementos foram removidos com sucesso.");
+                printf("\nPressione qualquer tecla para continuar...");
+                getchar();
+                system("cls");
+                return;
 
                 case 2:
-                excluirNodo(&((*atual)->right));
-                (*atual)->right = NULL;
-
-                break;
+                excluirNodo(&(*atual)->right); // Chama a função recursiva
+                (*atual)->right = NULL; // Deixa o endereço de memória a direita do atual como NULL
+                printf("\nOs elementos foram removidos com sucesso.");
+                printf("\nPressione qualquer tecla para continuar...");
+                getchar();
+                system("cls");
+                return;
 
                 case 3:
                 excluirNodo(&(*atual));
-                *raiz = NULL;
+                *raiz = NULL; // Deixa o endereço de memória raiz como NULL, deixando a árvore vazia
                 *atual = NULL;
-                break;
+                printf("\nOs elementos foram removidos com sucesso.");
+                printf("\nPressione qualquer tecla para continuar...");
+                getchar();
+                system("cls");
+                return;
         
                 default:
                 printf("\nOpcao invalida, digite novamente: ");
@@ -259,21 +290,49 @@ void removerElemento(Node **atual, Node **raiz) {
                 getchar();
                 break;
             }
-        }
-
-    
-
-    system("cls");
-    
+        }    
 }
 
+void listarOrdem(Node *atual, int nivel) {
+    if (atual == NULL) {
+        return; // Caso o valor atual seja nulo, a função termina e retorna para o nível anterior
+    }
+
+    listarOrdem(atual->right, nivel + 1); // A função começa listando todos os valores da direita, aumentando o nível a cada chamada recursiva
+
+    for (int i = 0; i < nivel; i++) {
+        printf("\t"); // Para cada nível de profundidade, será dado um tab, dessa forma fazendo com que seja possível visualizar a árvore independente do tamanho
+    }
+    printf("%d\n", atual->valor); // Será impresso o valor atual da árvore e irá pular para a próxima linha
+
+    listarOrdem(atual->left, nivel + 1); // Depois de listar todos os valores a direita, a função lista todos os valores a esquerda, aumentando o nível a cada chamada recursiva
+}
+
+void listarElementos(Node *raiz){
+    if (raiz == NULL) {
+        printf("\nA arvore esta vazia.");
+        printf("\nPressione qualquer tecla para continuar...");
+        getchar();
+        system("cls");
+        return;
+    }
+    else
+    {
+        listarOrdem(raiz, 0);
+        //Chama a função recursiva, passando a raiz e o nível sempre começará em 0 (raiz)
+    }
+    
+    printf("\nTodos os elementos da arvore.");
+    printf("\nPressione qualquer tecla para continuar...");
+    getchar();
+    system("cls");
+    return;
+}
 
 int main() {
 
-    Node *atual;
-    Node *raiz;
-    atual = NULL;
-    raiz = NULL;
+    Node *raiz = NULL;
+    Node *atual = NULL;
 
     int opcao, controleMenu;
 
@@ -300,13 +359,39 @@ int main() {
             inserirElemento(&raiz, &atual);
             break;
         case 2:
+
             system("cls");
-            printf("asdfuhasdihuoasohiuagsd");
-            elementoAtual(&atual);
+            if (atual == NULL){
+            printf("\nO elemento atual se encontra vazio.");
+            }
+            else{
+                printf("\nO elemento atual:\n");
+                printf("\n");
+                printf("\tAtual: %d\n", atual->valor);
+
+                    if (!(atual->left))
+                    {
+                    printf("\tEsquerda: #\n");
+                    }
+                    else{
+                    printf("\tEsquerda: %d\n", atual->left->valor);
+                    }
+                    if (!(atual->right))
+                    {
+                    printf("\tDireita: #\n");
+                    }
+                    else{
+                    printf("\tDireita: %d\n", atual->right->valor);
+                    }
+                }   
+                
+                printf("\nPressione qualquer tecla para continuar...");
+                getchar();
+                system("cls");
             break;
         case 3:
             system("cls");
-            percorrerArvore(&atual, &raiz);
+            percorrerArvore(&atual, raiz);
             break;
         case 4:
             system("cls");
@@ -318,7 +403,7 @@ int main() {
             break;
         case 6:
             system("cls");
-            //listarElementos();
+            listarElementos(raiz);
             break;
         case 0:
             printf("\nFinalizando o programa...\n");
